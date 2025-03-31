bc94ec263f9873e470742e0f56cf7b7c1e442161
Neysa Njo <nn6960@gadi-login-08.gadi.nci.org.au>
2025-03-31 19:53:37 +1100

    annotate combined fragments of d4z4

--- helper/add_colors_to_bed.py
+++ helper/add_colors_to_bed.py
@@ -1,5 +1,25 @@
 import argparse
+import pandas as pd
 
+def color_d4z4_chr4_proximal(main_tsv, outfile):
+    df = pd.read_csv(main_tsv, sep="\t")
+
+    for _, row in df.iterrows():
+        if not row["d4z4_chr4_proximal_mapped"]:
+            continue
+        
+        chrom = row["ReadID"]
+        start = row["d4z4_chr4_proximal_coords"].split("-")[0]
+        end = row["d4z4_chr4_proximal_coords"].split("-")[1]
+        score = row["d4z4_chr4_proximal_score"]
+        strand = row["strand"]
+        thick_start, thick_end = start, end
+        color = "255,0,0"
+        block_count, block_sizes, block_starts = 1, f"{int(end) - int(start)},", "0,"
+
+        outfile.write("\t".join([chrom, start, end, "d4z4_chr4_proximal", str(score), strand,
+            thick_start, thick_end, color, str(block_count),
+            block_sizes, block_starts]) + "\n")
 
 def process_beds(main_tsv, features_bed, repeats_bed, sslp_bed, output_bed):
     # Define color mapping
@@ -13,11 +33,19 @@ def process_beds(main_tsv, features_bed, repeats_bed, sslp_bed, output_bed):
     sslp_color = "255,0,255"  # Magenta for SSLP
 
     with open(main_tsv, "r") as tsv, open(features_bed, "r") as fbed, open(repeats_bed, "r") as rbed, open(output_bed, "w") as outfile, open(sslp_bed, "r") as sbed:
+
+        # Process d4z4_chr4_proximal
+        color_d4z4_chr4_proximal(main_tsv, outfile)
+        
         # Process feature BED entries
         for line in fbed:
             if line.strip():  # Skip empty lines
                 fields = line.strip().split("\t")
                 chrom, start, end, feature_name = fields[:4]
+
+                if feature_name == "d4z4_chr4_proximal":
+                    continue
+
                 score = fields[4] if len(fields) > 4 else "0"
                 strand = fields[5] if len(fields) > 5 else "+"
                 color = feature_color_map.get(feature_name, "0,0,0")  # Default to black
