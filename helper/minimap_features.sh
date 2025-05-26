715ca6f7377ab47f74acc3338ca54fa632172527
Neysa Njo <nn6960@gadi-login-05.gadi.nci.org.au>
2025-05-27 09:50:36 +1000

    editing tmp file to not be on shared area when running multiple samples

--- helper/minimap_features.sh
+++ helper/minimap_features.sh
@@ -12,44 +12,46 @@ SEQ_4QB=/g/data/kr68/neysa/fshd_pipeline/inputs/4qB_rc.fa
 # module load samtools/1.19
 # module load bedtools/2.28.0
 
-mkdir -p tmp_bams
+TEMP_BAM=$OUTDIR/tmp_bams
+mkdir -p $TEMP_BAM
 
-awk '/^>/ {if (seq) print seq > "tmp_read.fa"; print > "tmp_read.fa"; seq=""; next} {seq = seq $0} END {if (seq) print seq > "tmp_read.fa"}' "$READS"
+TMP_FA=$OUTDIR/tmp_read.fa
+awk '/^>/ {if (seq) print seq > "${TMP_FA}"; print > "${TMP_FA}"; seq=""; next} {seq = seq $0} END {if (seq) print seq > "${TMP_FA}"}' "$READS"
 
 while read -r header; do
     read_id=$(echo "$header" | cut -c2-)  # strip the leading ">"
     
     # Extract the single read into a temporary FASTA
-    awk -v h="$header" 'BEGIN {p=0} $0==h {p=1; print; next} /^>/ {p=0} p {print}' "$READS" > tmp_read.fa
+    awk -v h="$header" 'BEGIN {p=0} $0==h {p=1; print; next} /^>/ {p=0} p {print}' "$READS" > ${TMP_FA}
     
     # Run minimap2 using the read as the reference
-    minimap2 -x asm5 -t 1 -a tmp_read.fa "$FEATURES_FASTA" | \
+    minimap2 -x asm5 -t 1 -a ${TMP_FA} "$FEATURES_FASTA" | \
         samtools view -bS - | \
-        samtools sort -o "tmp_bams/${read_id}.bam"
+        samtools sort -o "${TEMP_BAM}/${read_id}.bam"
 
-    minimap2 -x asm5 -t 1 -a tmp_read.fa "$SEQ_4QB" | \
+    minimap2 -x asm5 -t 1 -a ${TMP_FA} "$SEQ_4QB" | \
         samtools view -bS - | \
-        samtools sort -o "tmp_bams/${read_id}_4qB.bam"
+        samtools sort -o "${TEMP_BAM}/${read_id}_4qB.bam"
 
     # Run minimap2 short for pLAM
-    minimap2 -x sr -t 1 -a tmp_read.fa "$PLAM_FASTA" | \
+    minimap2 -x sr -t 1 -a ${TMP_FA} "$PLAM_FASTA" | \
         samtools view -bS - | \
-        samtools sort -o "tmp_bams/${read_id}_short.bam"
+        samtools sort -o "${TEMP_BAM}/${read_id}_short.bam"
 
-    samtools merge -f "tmp_bams/${read_id}_merged.bam" \
-        "tmp_bams/${read_id}.bam" \
-        "tmp_bams/${read_id}_4qB.bam" \
-        "tmp_bams/${read_id}_short.bam"
+    samtools merge -f "${TEMP_BAM}/${read_id}_merged.bam" \
+        "${TEMP_BAM}/${read_id}.bam" \
+        "${TEMP_BAM}/${read_id}_4qB.bam" \
+        "${TEMP_BAM}/${read_id}_short.bam"
 
-    rm "tmp_bams/${read_id}.bam"
-    rm "tmp_bams/${read_id}_4qB.bam"
-    rm "tmp_bams/${read_id}_short.bam"
+    rm "${TEMP_BAM}/${read_id}.bam"
+    rm "${TEMP_BAM}/${read_id}_4qB.bam"
+    rm "${TEMP_BAM}/${read_id}_short.bam"
     
-    rm tmp_read.fa
+    rm ${TMP_FA}
 done < <(grep '^>' "$READS")
 
 # Merge all into a single BAM
-samtools merge -f "${OUTDIR}/${PREFIX}_mapped_features.merged.bam" tmp_bams/*.bam
+samtools merge -f "${OUTDIR}/${PREFIX}_mapped_features.merged.bam" ${TEMP_BAM}/*.bam
 samtools index "${OUTDIR}/${PREFIX}_mapped_features.merged.bam"
 
 bedtools bamtobed -i "${OUTDIR}/${PREFIX}_mapped_features.merged.bam" > "${OUTDIR}/${PREFIX}_mapped_features.merged.bed"
@@ -64,4 +66,4 @@ python3 /g/data/kr68/neysa/fshd_pipeline/helper/sam2psl.py \
 cat "${OUTDIR}/${PREFIX}_mapped_features.merged.psl" > "${OUTDIR}/${PREFIX}_mapped_features.psl"
 
 # Optional: clean up
-rm -r tmp_bams
+rm -r $TEMP_BAM
