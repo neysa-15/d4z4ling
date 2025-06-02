fcd429a57aa2e0cd3de976e438031b54d25cab42
Neysa Njo <nn6960@gadi-login-03.gadi.nci.org.au>
2025-06-02 11:08:06 +1000

    fix crossover temp directory

--- helper/minimap_features.sh
+++ helper/minimap_features.sh
@@ -13,28 +13,43 @@ SEQ_4QB=/g/data/kr68/neysa/fshd_pipeline/inputs/4qB_rc.fa
 # module load bedtools/2.28.0
 
 TEMP_BAM=$OUTDIR/tmp_bams
-mkdir -p $TEMP_BAM
+mkdir -p "$TEMP_BAM"
 
 TMP_FA=$OUTDIR/tmp_read.fa
-awk '/^>/ {if (seq) print seq > "${TMP_FA}"; print > "${TMP_FA}"; seq=""; next} {seq = seq $0} END {if (seq) print seq > "${TMP_FA}"}' "$READS"
+awk -v out="$TMP_FA" '
+    /^>/ {
+        if (seq) print seq >> out;
+        print >> out;
+        seq = "";
+        next
+    }
+    {
+        seq = seq $0
+    }
+    END {
+        if (seq) print seq >> out
+    }
+' "$READS"
 
 while read -r header; do
     read_id=$(echo "$header" | cut -c2-)  # strip the leading ">"
     
     # Extract the single read into a temporary FASTA
-    awk -v h="$header" 'BEGIN {p=0} $0==h {p=1; print; next} /^>/ {p=0} p {print}' "$READS" > ${TMP_FA}
+    TMP_READ_FA="${OUTDIR}/tmp_read_${read_id}.fa"
+    # awk -v h="$header" 'BEGIN {p=0} $0==h {p=1; print; next} /^>/ {p=0} p {print}' "$READS" > ${TMP_FA}
+    awk -v h="$header" 'BEGIN {p=0} $0==h {p=1; print; next} /^>/ {p=0} p {print}' "$TMP_FA" > ${TMP_READ_FA}
     
     # Run minimap2 using the read as the reference
-    minimap2 -x asm5 -t 1 -a ${TMP_FA} "$FEATURES_FASTA" | \
+    minimap2 -x asm5 -t 1 -N 10 -a ${TMP_READ_FA} "$FEATURES_FASTA" | \
         samtools view -bS - | \
         samtools sort -o "${TEMP_BAM}/${read_id}.bam"
 
-    minimap2 -x asm5 -t 1 -a ${TMP_FA} "$SEQ_4QB" | \
+    minimap2 -x asm5 -t 1 -a ${TMP_READ_FA} "$SEQ_4QB" | \
         samtools view -bS - | \
         samtools sort -o "${TEMP_BAM}/${read_id}_4qB.bam"
 
     # Run minimap2 short for pLAM
-    minimap2 -x sr -t 1 -a ${TMP_FA} "$PLAM_FASTA" | \
+    minimap2 -x sr -t 1 -N 10 -a ${TMP_READ_FA} "$PLAM_FASTA" | \
         samtools view -bS - | \
         samtools sort -o "${TEMP_BAM}/${read_id}_short.bam"
 
@@ -47,7 +62,7 @@ while read -r header; do
     rm "${TEMP_BAM}/${read_id}_4qB.bam"
     rm "${TEMP_BAM}/${read_id}_short.bam"
     
-    rm ${TMP_FA}
+    rm ${TMP_READ_FA}
 done < <(grep '^>' "$READS")
 
 # Merge all into a single BAM
@@ -66,4 +81,5 @@ python3 /g/data/kr68/neysa/fshd_pipeline/helper/sam2psl.py \
 cat "${OUTDIR}/${PREFIX}_mapped_features.merged.psl" > "${OUTDIR}/${PREFIX}_mapped_features.psl"
 
 # Optional: clean up
+rm $TMP_FA
 rm -r $TEMP_BAM
