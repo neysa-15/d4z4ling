8c5e0e7a8946942ae1f952c6bfa0d7d8b1f750f2
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2025-08-12 18:06:04 +1000

    update

--- gta100-wrapper.sh
+++ gta100-wrapper.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
+REALPATH=$(dirname "$(readlink -f "$0")")
+
+
 export PATH=$PATH:/install/merqury/meryl-1.4.1/bin/ #only for creatiob
 
-export PATH=$PATH:/data/hasindu/fshd_pipeline/install/winnowmap-v2.03/bin
-export PATH=$PATH:/install/seqkit-v2.3.0/
-export PATH=$PATH:/install/seqtk-1.5
-export PATH=$PATH:/install/ncbi-blast-2.17.0+/bin/
-export PATH=$PATH:/install/minimod-v0.4.0/
-export PATH=$PATH:/data/hasindu/fshd_pipeline/install/kentutils/
+export PATH=$PATH:${REALPATH}/install/winnowmap-v2.03/bin
+export PATH=$PATH:${REALPATH}/install/seqkit-v2.3.0/
+export PATH=$PATH:${REALPATH}/install/seqtk-1.5
+export PATH=$PATH:${REALPATH}/install/ncbi-blast-2.17.0+/bin/
+export PATH=$PATH:${REALPATH}/install/minimod-v0.4.0/
+export PATH=$PATH:${REALPATH}/install/kentutils/
 #samtools
 #bedtools
 #slow5-dorado
 #minimap2
 # python
-# minimod
 
 # check for all /data/hasindu paths and edit accordingly
 #check for all the relative paths and edit accordnly
 
+source ${REALPATH}/venv3/bin/activate
+# pip3 install pandas Bio pysam plotly scipy
 
-source /data/hasindu/fshd_pipeline/venv3/bin/activate
-# pip3 install pandas Bio pysam plotly
-
-#/usr/bin/time -v /install/slow5-dorado-0.8.3/bin/slow5-dorado basecaller --models-directory /install/dorado-0.8.3/models /install/dorado-0.8.3/models/dna_r10.4.1_e8.2_400bps_sup@v5.0.0 slow5-testdata/PGXXXX230339_reads_20k.blow5  --modified-bases 5mCG_5hmCG -x cuda:all > reads.bam
-
+#/usr/bin/time -v /install/slow5-dorado-0.8.3/bin/slow5-dorado basecaller --models-directory /install/dorado-0.8.3/models /install/dorado-0.8.3/models/dna_r10.4.1_e8.2_400bps_sup@v5.0.0 /data/hasindu/realfshd/test/data/PGXXXX230339_reads_20k.blow5  --modified-bases 5mCG_5hmCG -x cuda:all > reads.bam
 # 0:44.51
 
-/usr/bin/time -v /data/hasindu/fshd_pipeline/fshd1_script.sh --input-ubam /data/hasindu/fshd_pipeline/testdata/positive/AS2603/QGXHXX240243_supmeth_unaligned.bam
+#/usr/bin/time -v ${REALPATH}/fshd1_script.sh --input-ubam reads.bam
+/usr/bin/time -v ${REALPATH}/fshd1_script.sh --input-ubam  /data/hasindu/realfshd/test/data/positive/AS2603/QGXHXX240243_supmeth_unaligned.bam
 # 1:47.76
